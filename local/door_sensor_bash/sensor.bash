df78ab175cb959afcd3601c0c609a5b8cd678c3a
tomoyk <tmyk.kym@gmail.com>
2017-06-10 12:26:01 +0900

    add a function of first-doorOpen on day

--- local/door_sensor_bash/sensor.bash
+++ local/door_sensor_bash/sensor.bash
@@ -9,7 +9,9 @@ sudo echo $pinNum > /sys/class/gpio/export
 ### Set in-mode
 sudo echo in > /sys/class/gpio/gpio$pinNum/direction 
 
+### Set variables
 count=0
+slack=0
 
 while true
 do
@@ -17,34 +19,47 @@ do
   result=$(sudo cat /sys/class/gpio/gpio$pinNum/value)
   echo DoorState: $result
 
-  ### Door Open State
+  ### Check Door State
   if [ $result = "0" ]
   then
+
+    ### Door OPEN
     count=$(expr $count + 1)
+
   else
-    ### Reset Door State
+
+    ### Door CLOSE
     count=0
+
   fi
 
   ### Door Open
   if [ $count = "1" ]
   then
 
-    # echo "TRUE"
+    if [ $slack = 0 ]
+    then
 
-    TOKEN='YourTOKEN'
-    USER='doorBot'
-    CHANNEL='random'
-    MESSAGE='Door Opened DATE'
+      $slack=1
+      echo "First OPEN"
 
-#    curl -XPOST -d "token=$TOKEN" \
-#                -d "channel=#$CHANNEL" \
-#                -d "text=$MESSAGE" \
-#                -d "username=$USER" \
-#                "https://slack.com/api/chat.postMessage" &
+      ### First OPEN on day
+      TOKEN='YourTOKEN'
+      USER='doorBot'
+      CHANNEL='random'
+      MESSAGE='Door Opened DATE'
 
-#    mpg321 -g 1000 Anal/push.mp3 2>&1 > /dev/null
-#    mpg321 -g 1000 Anal/suiso.mp3 2>&1 > /dev/null
+    curl -XPOST -d "token=$TOKEN" \
+                -d "channel=#$CHANNEL" \
+                -d "text=$MESSAGE" \
+                -d "username=$USER" \
+                "https://slack.com/api/chat.postMessage" &
+
+    fi
+
+    mpg321 -g 1000 Anal/push.mp3 2>&1 > /dev/null
+    mpg321 -g 1000 Anal/suiso.mp3 2>&1 > /dev/null
+
   fi
   
   sleep 1
