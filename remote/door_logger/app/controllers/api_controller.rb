47fad984aaae9af97751a0be0fca5375298e005a
Kota Nonaka <kota.kaicho@gmail.com>
2017-07-06 19:12:20 +0900

    Add parameter to log api

--- remote/door_logger/app/controllers/api_controller.rb
+++ remote/door_logger/app/controllers/api_controller.rb
@@ -6,7 +6,8 @@ class ApiController < ApplicationController
   end
   
   def show
-    @log = Log.order(id: "DESC").limit(20)
+    count = params.has_key?(:count) ? params['count'].to_i() : 20
+    @log = Log.order(id: "DESC").limit(count)
     render json: @log
   end
 end
