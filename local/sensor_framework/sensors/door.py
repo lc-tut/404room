3a2a63bafc1fd340ac25eff42e0770e09f16424e
mute1997 <mute1008@gmail.com>
2017-07-11 11:14:21 +0900

    fixes #3

--- local/sensor_framework/sensors/door.py
+++ local/sensor_framework/sensors/door.py
@@ -1,31 +1,23 @@
 from concurrent.futures import ThreadPoolExecutor
 from sensors.sensor import Sensor
 import time
+import socket
 
 class Door(Sensor):
     def __init__(self, store):
-        self.store = store
-        self.sleep_time = 1
-        self.pin_num = 4
-
-        with open('/sys/class/gpio/export', 'w') as f:
-            f.write(self.pin_num)
-
-        with open('/sys/class/gpio/gpio%s/direction' % self.pin_num, 'w') as f:
-            f.write("in")
+        super().__init__(store)
+        self.door_socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
 
     async def start(self):
+        self.door_socket.bind('/tmp/door.sock')
+        self.door_socket.listen(1)
+
         while True:
+            conn, addr = self.door_socket.accept()
             try:
-                pin_state = await self.__get_pin_state()
-                if (pin_state == 0):
-                    self.execute_plugins()
-                    print("Door Open")
-            except Exception as e:
-                print(e)
-
-            time.sleep(self.sleep_time)
-
-    async def __get_pin_state(self):
-        with open('/sys/class/gpio/gpio%s/value' % self.pin_num, 'r') as f:
-            return int(f.read())
+                while True:
+                    data = conn.recv(1024)
+                    self.store.set('Door',{'count', Data},self.execute_plugins)
+            except:
+                conn.close()
+                os.remove(SOCK_FILENAME)
